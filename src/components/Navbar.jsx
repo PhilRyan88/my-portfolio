955922cfea633e6dd6c5d2d815fafce2f1fcb94c
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2025-03-15 22:34:43 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,41 @@
 import React, { useRef, useEffect } from "react";
 import "../styles/Navbar.css";
 import { useNavigate } from "react-router-dom";
-
+import Works from "./Works";
 import { Power3, gsap } from "gsap";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const items = useRef(null);
   useEffect(() => {
-    gsap.to(items.current, {
-      x: 20,
-      duration: 1,
+    gsap.from(".li", {
+      y: -20,
+      duration: 0.2,
+      opacity: 0,
+      ease: Power3.in,
+    });
+    gsap.to(".li", {
+      opacity: 1,
+      y: 25,
+      duration: 0.8,
 
-      delay: 0.5,
-      opacity: 2,
+      stagger: 0.3,
       ease: Power3.ease,
     });
   }, []);
   return (
     <div className="nav-container">
       <ul ref={items} className="nav-items">
-        <li>Home</li>
-        <li>
+        <li className="li">Home</li>
+        <li className="li">
           <button className="service-btn" onClick={() => navigate("/services")}>
             Services
           </button>
         </li>
-        <li>Works</li>
-        <li>Connect</li>
+        <li className="li">Works</li>
+        <li className="li">Connect</li>
       </ul>
+      <Works />
     </div>
   );
 };
